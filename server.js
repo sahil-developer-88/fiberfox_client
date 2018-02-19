6020e3da799371eb07e66183183037765c82c1e4
sahil-developer-88 <sahiljain12345@gmail.com>
2018-02-19 15:52:51 +0530

    update changes

---
+++ server.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const http = require('http');
+const app = express();
+
+// API file for interacting with MongoDB
+// const api = require('./server/routes/api');
+
+// session
+var cookieParser = require('cookie-parser');
+var session = require('express-session');
+app.use(cookieParser());
+app.use(session({
+    secret: 'sessionTesting',
+    saveUninitialized: false,
+    resave: false
+})); 
+
+
+
+// Parsers
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false}));
+
+// Angular DIST output folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// API location
+// app.use('/api', api);
+
+// Send all other requests to the Angular app
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/index.html'));
+});
+
+//Set Port
+const port = process.env.PORT || '8080';
+app.set('port', port);
+
+const server = http.createServer(app);
+
+server.listen(port, () => console.log(`Running on localhost:${port}`));
